d695c92a675269a917955ffde786087595b77973
Tolkynbayeva <workfortime37@gmail.com>
2024-01-26 03:55:53 +0600

    getting location data from across platforms

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,47 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+ void getlocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Проверяем, включены ли службы геолокации
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Если службы геолокации выключены, можете вывести диалоговое окно с запросом включения
+      // или перенаправить пользователя в настройки устройства.
+      return;
+    }
+
+    // Запрашиваем разрешение на использование местоположения
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Если пользователь отказал в предоставлении разрешения, вы можете предпринять соответствующие действия
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Пользователь навсегда отклонил разрешение, вы можете предпринять соответствующие действия
+      return;
+    }
+
+    // Теперь, когда у нас есть разрешение, мы можем получить местоположение
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+    );
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
+        child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getlocation();
           },
           child: Text('Get Location'),
         ),
