fb9f19b03a1837c9ef22ce5a6d329d40179befd1
Tolkynbayeva <workfortime37@gmail.com>
2024-02-03 04:49:24 +0600

    add location.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,44 +7,17 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  @override void initState() {
+  @override
+  void initState() {
     super.initState();
     getlocation();
   }
-  
-  
+
   void getlocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Проверяем, включены ли службы геолокации
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Если службы геолокации выключены, можете вывести диалоговое окно с запросом включения
-      // или перенаправить пользователя в настройки устройства.
-      return;
-    }
-
-    // Запрашиваем разрешение на использование местоположения
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Если пользователь отказал в предоставлении разрешения, вы можете предпринять соответствующие действия
-        return;
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Пользователь навсегда отклонил разрешение, вы можете предпринять соответствующие действия
-      return;
-    }
-
-    // Теперь, когда у нас есть разрешение, мы можем получить местоположение
-    Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.low,
-    );
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
   }
 
   @override
